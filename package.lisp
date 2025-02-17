7c736f950f8182ef29571401e73a28c735fed98f
Joe Marshall <51824598+jrm-code-project@users.noreply.github.com>
2025-02-17 15:04:39 -0800

    Update package.lisp

--- package.lisp
+++ package.lisp
@@ -2,5 +2,5 @@
 
 (defpackage "FOLD"
   (:documentation "Implementation of FOLD-LEFT and FOLD-RIGHT.")
-  (:export "FOLD-LEFT" "FOLD-RIGHT")
+  (:export "FOLD-LEFT" "FOLD-RIGHT" "*TRUNCATE-FOLD*")
   (:use "COMMON-LISP"))
